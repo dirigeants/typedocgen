79d3480ca18f9b7af77cc8a3b504c24261475bb0
Jacz <itsmejacz@gmail.com>
2020-05-27 01:16:21 +1000

    Squash bugs

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -2,7 +2,7 @@ import { config as cliargs } from './Config';
 import { generateTypedocJson } from './util/TypedocJsonGenerator';
 import { relative, join, dirname } from 'path';
 import { readJSON, outputFile } from 'fs-nextra';
-import { readFile } from 'fs/promises';
+import { promises } from 'fs';
 import { DocConfig, DocConfigCategory, DocConfigFile } from './util/Types';
 
 export class Cli {
@@ -12,10 +12,13 @@ export class Cli {
 	public fileCount = 0;
 
 	public async run(): Promise<never> {
+		console.log(`Currently generating typedoc json in all source directories`);
 		const typedoc = generateTypedocJson(cliargs.source);
 		if (!typedoc) return process.exit(1);
+
 		if (cliargs.config) {
 			await this.loadGuides();
+			console.log(`${this.fileCount} custom docs file${this.fileCount !== 1 ? 's' : ''} in ${this.categoryCount} categor${this.categoryCount !== 1 ? 'ies' : 'y'} loaded.`);
 			(typedoc as any).guides = this.guides;
 		}
 
@@ -36,19 +39,17 @@ export class Cli {
 
 	public parseCategory(category: DocConfigCategory): Promise<void[]> {
 		const dir = join(dirname(cliargs.config), category.path || category.name);
-
-		this.guides[category.name] = { name: category.name, files: [] };
+		this.guides[category.name] = { name: category.name, files: {} };
 		this.categoryCount++;
 
-		return Promise.all(category.files.map(file => this.parseFile(dir, file, category)));
+		return Promise.all(category.files.map(file => this.parseFile(join(dir, file.path), file, category)));
 	}
 
 	public async parseFile(filePath: string, file: DocConfigFile, category: DocConfigCategory): Promise<void> {
-		const content = await readFile(filePath, 'utf-8');
 		const path = relative(cliargs.root, filePath).replace(/\\/g, '/');
+		const content = await promises.readFile(filePath, 'utf-8');
 
 		if (cliargs.verbose) console.log(`Loaded custom docs file ${category.name}/${file.name}`);
-
 		this.guides[category.name].files[file.path] = { name: file.name, content, path };
 		this.fileCount++;
 	}
